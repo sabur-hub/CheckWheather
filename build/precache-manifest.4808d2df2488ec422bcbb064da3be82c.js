beeada01112da242914b9bdd93d23bd32b9ff3bb
Outic <76915977+sabur-hub@users.noreply.github.com>
2023-01-09 05:36:43 +0300

    wheather app
    souce and build proj

---
+++ build/precache-manifest.4808d2df2488ec422bcbb064da3be82c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f76af1eed9a07243fabd9afb6bf0d44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b37ba32fee4d5ef07d2",
+    "url": "/static/css/main.03e6d2e5.chunk.css"
+  },
+  {
+    "revision": "6f619f27ee849183fe28",
+    "url": "/static/js/2.23aafde6.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.23aafde6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b37ba32fee4d5ef07d2",
+    "url": "/static/js/main.8bdfbae1.chunk.js"
+  },
+  {
+    "revision": "ae594932184f88bfb438",
+    "url": "/static/js/runtime-main.9d9d98d4.js"
+  }
+]);
